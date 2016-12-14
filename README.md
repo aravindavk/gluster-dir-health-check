7072393c946c523b874c133d926e8e97a898c4ac
Aravinda VK <mail@aravindavk.in>
2016-12-14 11:47:28 +0530

    Initial working patch.
    Signed-off-by: Aravinda VK <mail@aravindavk.in>

---
+++ README.md
@@ -0,0 +1,39 @@
+# Gluster Directories Health Check
+
+A tool to detect stale or boken directories in Gluster bricks. This tool can detect following issues
+
+**No GFID**: If a directory in brick backend doesn't have `trusted.gfid` xattr  
+**No Parent GFID**: Parent directory doesn't have a `trusted.gfid` xattr  
+**No Symlink**: Gluster maintains a symlink in `$BRICK/.glusterfs` directory for each directory, If Symlink file not present for a directory  
+**Wrong Symlink**: If symlink exists but linked to different directory  
+**Invalid GFID**: Invalid data in `trusted.gfid` xattr  
+**Invalid Parent GFID**: Invalid data in `trusted.gfid` xattr of parent dir  
+
+## Install
+
+If Rust is installed in your machine then run the following
+
+    cargo install gluster_dir_health_check --git https://github.com/aravindavk/gluster_dir_health_check
+
+## Usage
+
+    gluster_dir_health_check <brick_path>
+
+Example:
+
+    gluster_dir_health_check /bricks/b1
+    gluster_dir_health_check /bricks/b1 > ~/bricks_b1_dirs_status.txt
+
+## Output
+
+Example output,
+
+    STATUS   DESCRIPTION   GFID                                 PGFID                                PATH
+    -------- ------------- ------------------------------------ ------------------------------------ ---------------------
+    [OK    ]               0b875a91-1a51-42fc-b68a-83c9248bdbb8 00000000-0000-0000-0000-000000000001 /bricks/b1/d1
+    [OK    ]               a6e870e4-4376-493c-96ca-678f0e1d01fe 00000000-0000-0000-0000-000000000001 /bricks/b1/n1
+    [NOT OK] WRONG SYMLINK a6e870e4-4376-493c-96ca-678f0e1d01fe 00000000-0000-0000-0000-000000000001 /bricks/b1/h1
+    [OK    ]               bac0c3fb-26c8-403b-85ab-e344b7c10011 00000000-0000-0000-0000-000000000001 /bricks/b1/md1
+    [OK    ]               9bef6669-ac60-48c0-8599-bd6dd1909957 bac0c3fb-26c8-403b-85ab-e344b7c10011 /bricks/b1/md1/sd1
+
+Above output shows GFID `a6e870e4-4376-493c-96ca-678f0e1d01fe` is associated with two directories `/bricks/b1/n1` and `/bricks/b1/h1`. But backend symlink is with associated with `/bricks/b1/n1`
---
+++ src/main.rs
@@ -0,0 +1,151 @@
+use std::path::Path;
+use std::io::Error;
+use std::env;
+
+extern crate walkdir;
+extern crate xattr;
+extern crate uuid;
+
+use walkdir::{DirEntry, WalkDir, WalkDirIterator};
+use uuid::Uuid;
+
+fn filter_dirs(en: &DirEntry) -> bool {
+    // To ignore internal directories
+    en.file_name()
+        .to_str()
+        .map(|s| s.starts_with(".glusterfs") || s.starts_with(".trashcan"))
+        .unwrap_or(false)
+}
+
+fn get_gfid(path: &str) -> Result<String, Error> {
+    // Gets trusted.gfid xattr from the given path, Returns
+    // empty string if UUID parsing fails
+    let v = try!(xattr::get(path, "trusted.gfid"));
+    let uuid = Uuid::from_bytes(&v);
+    match uuid {
+        Ok(v) => Ok(v.hyphenated().to_string()),
+        Err(_) => Ok("".to_string()),
+    }
+}
+
+// All output combinations
+enum Reasons {
+    Ok,
+    NoGfid,
+    NoPgfid,
+    NoSymlink,
+    WrongSymlink,
+    InvalidGfid,
+    InvalidPGfid,
+}
+
+fn output_display(reason: Reasons, entry: &DirEntry, gfid: &str, pgfid: &str) {
+    let ok_msg = match reason {
+        Reasons::Ok => "[    OK]              ",
+        Reasons::NoGfid => "[NOT OK] NO GFID      ",
+        Reasons::NoPgfid => "[NOT OK] NO PGFID     ",
+        Reasons::NoSymlink => "[NOT OK] NO SYMLINK   ",
+        Reasons::WrongSymlink => "[NOT OK] WRONG SYMLINK",
+        Reasons::InvalidGfid => "[NOT OK] INVALID GFID ",
+        Reasons::InvalidPGfid => "[NOT OK] INVALID PGFID",
+    };
+    println!("{} {:36} {:36} {}",
+             ok_msg,
+             gfid,
+             pgfid,
+             entry.path().display());
+}
+
+// path_is_symbolic_link
+
+fn get_paths(path: &str) {
+    println!("STATUS   DESCRIPTION   {:36} {:36} PATH", "GFID", "PGFID");
+    println!("-------- ------------- ------------------------------------ \
+              ------------------------------------ ---------------------");
+
+    // Recursive crawl
+    for entry in WalkDir::new(path).into_iter().filter_entry(|e| !filter_dirs(e)) {
+        let entry = entry.unwrap();
+
+        // Ignore Brick Root
+        if entry.path().to_str().unwrap() == path {
+            continue;
+        }
+
+        // If it is a directory
+        if entry.file_type().is_dir() {
+            // Collect the GFID of dir
+            let gfid = match get_gfid(entry.path().to_str().unwrap()) {
+                Ok(v) => {
+                    if v == "".to_string() {
+                        output_display(Reasons::InvalidGfid, &entry, "", "");
+                        continue;
+                    }
+                    v
+                }
+                Err(_) => {
+                    output_display(Reasons::NoGfid, &entry, "", "");
+                    continue;
+                }
+            };
+
+            // Construct the gfid path as
+            // $BRICK/.glusterfs/<gfid[0:2]>/<gfid[2:4]>/<gfid>
+            let (first_two, last) = gfid.split_at(2);
+            let (next_two, _) = last.split_at(2);
+            let gfid_path = Path::new(path)
+                .join(".glusterfs")
+                .join(first_two)
+                .join(next_two)
+                .join(&gfid);
+
+            // Get Parent Dir GFID
+            let pgfid = match get_gfid(entry.path().parent().unwrap().to_str().unwrap()) {
+                Ok(v) => {
+                    if v == "".to_string() {
+                        output_display(Reasons::InvalidPGfid, &entry, &gfid, "");
+                        continue;
+                    }
+                    v
+                }
+                Err(_) => {
+                    output_display(Reasons::NoPgfid, &entry, &gfid, "");
+                    continue;
+                }
+            };
+
+            // Construct the pgfid path to compare with symlink target
+            // ../../<pgfid[0:2]>/<pgfid[2:4]>/<pgfid>/<dirname>
+            let (first_two_pgfid, last) = pgfid.split_at(2);
+            let (next_two_pgfid, _) = last.split_at(2);
+            let pgfid_path = Path::new("../../")
+                .join(first_two_pgfid)
+                .join(next_two_pgfid)
+                .join(&pgfid)
+                .join(entry.file_name().to_str().unwrap());
+
+            // Try reading the symlink, All good if read link matches with expected pgfid_path
+            // If target is different then it may have wrongly linked to different directory
+            match gfid_path.read_link() {
+                Ok(v) => {
+                    if v == pgfid_path {
+                        output_display(Reasons::Ok, &entry, &gfid, &pgfid);
+                    } else {
+                        output_display(Reasons::WrongSymlink, &entry, &gfid, &pgfid);
+                    }
+                }
+                Err(_) => {
+                    output_display(Reasons::NoSymlink, &entry, &gfid, &pgfid);
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut args = env::args();
+    match args.nth(1) {
+        Some(v) => get_paths(&v),
+        None => println!("Brick path is required"),
+    }
+}
